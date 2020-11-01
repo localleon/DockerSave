ba9cae80f319a8390a3afd3ff8a44c70d7c7b041
localleon <me@lrau.xyz>
2020-11-01 20:14:27 +0100

    Added Layer Blob Download, still WIP

--- api.go
+++ api.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 type AuthToken struct {
 	Token       string `json:token`
 	AccessToken string `json:access_token`
@@ -29,9 +31,108 @@ type Manifest struct {
 		Size      int    `json:"size"`
 		Digest    string `json:"digest"`
 	} `json:"config"`
-	Layers []struct {
-		MediaType string `json:"mediaType"`
-		Size      int    `json:"size"`
-		Digest    string `json:"digest"`
-	} `json:"layers"`
+	Layers []Layer `json:"layers"`
+}
+
+type Layer struct {
+	MediaType string `json:"mediaType"`
+	Size      int    `json:"size"`
+	Digest    string `json:"digest"`
+}
+
+type ImageConfig struct {
+	Architecture string `json:"architecture"`
+	Config       struct {
+		Hostname     string   `json:"Hostname"`
+		Domainname   string   `json:"Domainname"`
+		User         string   `json:"User"`
+		AttachStdin  bool     `json:"AttachStdin"`
+		AttachStdout bool     `json:"AttachStdout"`
+		AttachStderr bool     `json:"AttachStderr"`
+		Tty          bool     `json:"Tty"`
+		OpenStdin    bool     `json:"OpenStdin"`
+		StdinOnce    bool     `json:"StdinOnce"`
+		Env          []string `json:"Env"`
+		Cmd          []string `json:"Cmd"`
+		ArgsEscaped  bool     `json:"ArgsEscaped"`
+		Image        string   `json:"Image"`
+		Volumes      struct {
+			Git struct {
+			} `json:"/git"`
+		} `json:"Volumes"`
+		WorkingDir string      `json:"WorkingDir"`
+		Entrypoint []string    `json:"Entrypoint"`
+		OnBuild    interface{} `json:"OnBuild"`
+		Labels     struct {
+			Maintainer string `json:"maintainer"`
+		} `json:"Labels"`
+	} `json:"config"`
+	Container       string `json:"container"`
+	ContainerConfig struct {
+		Hostname     string   `json:"Hostname"`
+		Domainname   string   `json:"Domainname"`
+		User         string   `json:"User"`
+		AttachStdin  bool     `json:"AttachStdin"`
+		AttachStdout bool     `json:"AttachStdout"`
+		AttachStderr bool     `json:"AttachStderr"`
+		Tty          bool     `json:"Tty"`
+		OpenStdin    bool     `json:"OpenStdin"`
+		StdinOnce    bool     `json:"StdinOnce"`
+		Env          []string `json:"Env"`
+		Cmd          []string `json:"Cmd"`
+		ArgsEscaped  bool     `json:"ArgsEscaped"`
+		Image        string   `json:"Image"`
+		Volumes      struct {
+			Git struct {
+			} `json:"/git"`
+		} `json:"Volumes"`
+		WorkingDir string      `json:"WorkingDir"`
+		Entrypoint []string    `json:"Entrypoint"`
+		OnBuild    interface{} `json:"OnBuild"`
+		Labels     struct {
+			Maintainer string `json:"maintainer"`
+		} `json:"Labels"`
+	} `json:"container_config"`
+	Created       time.Time `json:"created"`
+	DockerVersion string    `json:"docker_version"`
+	History       []struct {
+		Created    time.Time `json:"created"`
+		CreatedBy  string    `json:"created_by"`
+		EmptyLayer bool      `json:"empty_layer,omitempty"`
+	} `json:"history"`
+	Os     string `json:"os"`
+	Rootfs struct {
+		Type    string   `json:"type"`
+		DiffIds []string `json:"diff_ids"`
+	} `json:"rootfs"`
+}
+
+type ContentManifest struct {
+	Config   string   `json:"Config"`
+	RepoTags []string `json:RepoTags`
+	Layers   []string `json:Layers`
+}
+
+type LayerJson struct {
+	ID              string    `json:id`
+	Created         time.Time `json:"created"`
+	ContainerConfig struct {
+		Hostname     string      `json:"Hostname"`
+		Domainname   string      `json:"Domainname"`
+		User         string      `json:"User"`
+		AttachStdin  bool        `json:"AttachStdin"`
+		AttachStdout bool        `json:"AttachStdout"`
+		AttachStderr bool        `json:"AttachStderr"`
+		Tty          bool        `json:"Tty"`
+		OpenStdin    bool        `json:"OpenStdin"`
+		StdinOnce    bool        `json:"StdinOnce"`
+		Env          interface{} `json:"Env"`
+		Cmd          interface{} `json:"Cmd"`
+		Image        string      `json:"Image"`
+		Volumes      interface{} `json:"Volumes"`
+		WorkingDir   string      `json:"WorkingDir"`
+		Entrypoint   interface{} `json:"Entrypoint"`
+		OnBuild      interface{} `json:"OnBuild"`
+		Labels       interface{} `json:"Labels"`
+	} `json:"container_config"`
 }
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 var authService string = "registry.docker.io"
@@ -15,20 +20,16 @@ var authResult AuthToken
 func main() {
 	api = &http.Client{}
 
+	// Define default values for image download
 	image := "alpine/git"
 	tag := "latest"
 	authScope := "repository:" + image + ":pull"
 
-	fmt.Println("Getting Authenticaton Token")
 	// Get Authentication Token for authScope
-	uri := "https://auth.docker.io/token?service=" + authService + "&scope=" + authScope
-	resp, err := http.Get(uri)
-	if err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println("Getting Authenticaton Token")
+	authResult = getAuthToken(authService, authScope)
 
-	json.NewDecoder(resp.Body).Decode(&authResult)
-
+	// Getting Information
 	fmt.Println("Retrieving Information about Container Image")
 	infos := getManifestInfos(image, tag)
 	amd64 := infos.Manifests[0]
@@ -36,8 +37,88 @@ func main() {
 	// Download Manifest
 	fmt.Println("Downloading Manifest Files")
 	manifest := downloadManifest(image, amd64.Digest)
-	writeManifestToFile(manifest, "./golayer/manifest.json")
+	// writeManifestToFile(manifest, "./golayer/manifest.json")
 
+	// Create Image Folder
+	err := os.Mkdir("./golayer/", 0777)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	// Downloading Config Json
+	config := downloadConfig(image, manifest.Config.Digest)
+	configFile, _ := json.MarshalIndent(config, "", " ")
+	configFileName := manifest.Config.Digest[7:] + ".json"
+	_ = ioutil.WriteFile("./golayer/"+configFileName, configFile, 0644)
+
+	// Build Content Manifest File
+	var contentManifest ContentManifest
+	contentManifest.Config = configFileName
+	contentManifest.RepoTags = []string{image + ":" + tag}
+
+	// Build Layers
+	var parentID string
+
+	l := manifest.Layers[0]
+	blob := l.Digest
+	// Create first Layer Folder
+	fakeLayerID := generateFakeID(parentID, blob)
+	_ = os.Mkdir("./golayer/"+fakeLayerID, 0777)
+	downloadLayerBlob(image, blob, fakeLayerID)
+
+	// Create Version File
+	f, err := os.Create("./golayer/" + fakeLayerID + "/VERSION")
+	f.Write([]byte("1.0"))
+	f.Close()
+
+	// Create JSON File for Layer
+	var json LayerJson
+	json.Created = config.Created // Set Creation date
+	json.ID = fakeLayerID
+
+}
+
+func downloadLayerBlob(image, blob, fakeLayerID string) {
+	uri := registryURL + "/v2/" + image + "/blobs/" + blob
+	reqInfo, err := http.NewRequest("GET", uri, nil)
+	reqInfo.Header.Add("Authorization", "Bearer "+authResult.Token)
+	reqInfo.Header.Add("Accept", "application/vnd.docker.distribution.manifest.list.v2+json")
+	resp, err := api.Do(reqInfo)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	out, err := os.Create("./golayer/" + fakeLayerID + "/layer.tar")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	fmt.Println(err)
+}
+
+func generateFakeID(id, b string) string {
+	s := id + "\n" + b + "\n"
+	h := hmac.New(sha256.New, []byte("secret"))
+	h.Write([]byte(s))
+
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func downloadConfig(image, digest string) ImageConfig {
+	uri := registryURL + "/v2/" + image + "/blobs/" + digest
+	reqInfo, err := http.NewRequest("GET", uri, nil)
+	reqInfo.Header.Add("Authorization", "Bearer "+authResult.Token)
+	reqInfo.Header.Add("Accept", "application/vnd.docker.distribution.manifest.list.v2+json")
+	resp, err := api.Do(reqInfo)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var result ImageConfig
+	json.NewDecoder(resp.Body).Decode(&result)
+	return result
 }
 
 func writeManifestToFile(m Manifest, path string) {
@@ -72,6 +153,18 @@ func getManifestInfos(image, tag string) ManifestInfo {
 	}
 	// Decode Manifest infos
 	var result ManifestInfo
+	json.NewDecoder(resp.Body).Decode(&result)
+	return result
+}
+
+func getAuthToken(service, scope string) AuthToken {
+	uri := "https://auth.docker.io/token?service=" + service + "&scope=" + scope
+	resp, err := http.Get(uri)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var result AuthToken
 	json.NewDecoder(resp.Body).Decode(&result)
 	return result
 }
